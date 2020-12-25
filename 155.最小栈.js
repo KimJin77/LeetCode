3e8c5370427cca68667ead23429cf8f94bb4a1d7
金烁 <shuo_jin@kingdee.com>
2020-12-25 09:30:51 +0800

    完成 [155] 最小栈

--- 155.最小栈.js
+++ 155.最小栈.js
@@ -9,7 +9,8 @@
  * initialize your data structure here.
  */
 var MinStack = function() {
-
+  this.stack = [];
+  this.minStack = [];
 };
 
 /** 
@@ -17,28 +18,41 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) {
-
+  this.stack.push(x);
+  if (this.minStack.length === 0) {
+    this.minStack.push(x);
+  } else {
+    const prevMin = this.minStack[this.minStack.length - 1];
+    this.minStack.push(prevMin > x ? x : prevMin);
+  }
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-
+  this.stack.pop();
+  this.minStack.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-
+  if (this.stack.length !== 0) {
+    return this.stack[this.stack.length - 1];
+  }
+  return null;
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-
+  if (this.minStack.length !== 0) {
+    return this.minStack[this.minStack.length - 1];
+  }
+  return null;
 };
 
 /**
